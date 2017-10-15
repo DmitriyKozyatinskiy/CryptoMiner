224c41755ab3f406e42ed6e61cbb612edfcee4d7
DmitriyKozyatinskiy <dmitriy.kozyatinskiy@gmail.com>
2017-10-15 23:43:50 +0300

    connect background with popup

--- src/js/popup.js
+++ src/js/popup.js
@@ -1,4 +1,4 @@
-import '../css/popup.css';
+import './../css/popup.css';
 import {
   IS_MINING,
   ENABLE_MINING,
@@ -6,18 +6,25 @@ import {
   GET_SETTINGS,
   SAVE_SETTINGS,
   GET_HASHES_PER_SECOND,
+  HASH_UPDATE_INTERVAL,
 } from './constants';
 
+const { runtime } = chrome;
 const miningButton = document.getElementById('miningButton');
+const saveButton = document.getElementById('saveButton');
+const siteKeyInput = document.getElementById('siteKeyInput');
+const threadsInput = document.getElementById('threadsInput');
+const throttleInput = document.getElementById('throttleInput');
+const hashesPerSecondsAmount = document.getElementById('hashesPerSecondsAmount');
 
 const setMiningStatus = () => {
-  chrome.runtime.sendMessage({ type: IS_MINING }, (isMining) => {
+  runtime.sendMessage({ type: IS_MINING }, (isMining) => {
     if (isMining) {
-      chrome.runtime.sendMessage({ type: DISABLE_MINING }, () => {
+      runtime.sendMessage({ type: DISABLE_MINING }, () => {
         disableMining();
       });
     } else {
-      chrome.runtime.sendMessage({ type: ENABLE_MINING }, () => {
+      runtime.sendMessage({ type: ENABLE_MINING }, () => {
         enableMining();
       });
     }
@@ -26,13 +33,27 @@ const setMiningStatus = () => {
 
 const enableMining = () => {
   miningButton.innerText = 'STOP MINING';
+  miningButton.classList.remove('button-run');
+  miningButton.classList.add('button-stop');
 };
 
 const disableMining = () => {
   miningButton.innerText = 'START MINING';
+  miningButton.classList.remove('button-stop');
+  miningButton.classList.add('button-run');
 };
 
-chrome.runtime.sendMessage({ type: IS_MINING }, (isMining) => {
+const saveSettings = () => {
+  const siteKey = siteKeyInput.value;
+  const threads = threadsInput.value;
+  const throttle = throttleInput.value;
+  const data = {
+    settings: { siteKey, threads, throttle },
+  };
+  runtime.sendMessage({ type: SAVE_SETTINGS, data });
+};
+
+runtime.sendMessage({ type: IS_MINING }, (isMining) => {
   if (isMining) {
     enableMining();
   } else {
@@ -40,4 +61,17 @@ chrome.runtime.sendMessage({ type: IS_MINING }, (isMining) => {
   }
 });
 
+runtime.sendMessage({ type: GET_SETTINGS }, ({ siteKey, threads, throttle }) => {
+  siteKeyInput.value = siteKey;
+  threadsInput.value = threads;
+  throttleInput.value = throttle;
+});
+
+setInterval(function() {
+  runtime.sendMessage({ type: GET_HASHES_PER_SECOND }, (hashes) => {
+    hashesPerSecondsAmount.innerText = hashes === 0 ? hashes : hashes.toFixed(1);
+  });
+}, HASH_UPDATE_INTERVAL);
+
 miningButton.addEventListener('click', () => setMiningStatus());
+saveButton.addEventListener('click', () => saveSettings());
